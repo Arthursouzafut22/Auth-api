6c304331e3210f6381fe742eb46c8eb6b1291644
Arthursouzafut22 <arthursouzafut22@gmail.com>
2025-07-27 10:25:33 -0300

    Criando rota para cadastrar usuário

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import conexao from "./models/conexao.js";
 
 dotenv.config();
@@ -9,10 +11,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
-  res.json({ teste: "Olá" });
+  res.status(200).json({ teste: "Olá" });
+});
+
+app.post("/register", async (req, res) => {
+  const body = req.body;
+  const nome = body.Nome;
+  const email = body["E-mail"];
+  const senha = body.Senha;
+  const confime = body["Confirme a senha"];
+
+  if (!nome) {
+    return res.status(244).json({ mensagem: "O nome e obrigatorio" });
+  }
+  if (!email) {
+    return res.status(244).json({ mensagem: "O e-mail e obrigatorio" });
+  }
+  if (!senha) {
+    return res.status(244).json({ mensagem: "A senha e obrigatorio" });
+  }
+  if (senha !== confime) {
+    return res.status(244).json({ mensagem: "As senhas estão diferentes" });
+  }
+
+  const [rows] = await conexao.execute(
+    "SELECT email from users WHERE email = ?",
+    [email]
+  );
+
+  if (rows.length > 0) {
+    return res.status(244).json({ mensagem: "Esse email ja esta cadastrado" });
+  }
+
+  const salt = await bcrypt.genSalt(12);
+  const senhaHash = await bcrypt.hash(senha, salt);
+
+  await conexao.execute("INSERT INTO users (nome,email,senha) VALUES(?,?,?)", [
+    nome,
+    email,
+    senhaHash,
+  ]);
+  res.status(201).json({ sucesso: "usuário cadastrado com sucesso!" });
 });
 
 app.listen(PORT, () => console.log("Servidor rodando!!!"));
--- src/models/conexao.js
+++ src/models/conexao.js
@@ -1,15 +1,17 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+
 
 dotenv.config();
 
-const conexao = mysql.createConnection({
+const conexao = await mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
+
 
 conexao.connect((err) => {
   if (err) {
